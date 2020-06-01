fe147c80a685f5bd763e003bc4e89055fe2f8e2a
Anton Zadora <az@visuals.ru>
2020-06-01 18:21:35 +0300

    prefabs

---
+++ Assets/PUN tutorial/PlayerUI.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MyGame
+{
+    public class PlayerUI : MonoBehaviour
+    {
+        #region Private Fields
+
+
+        [Tooltip("UI Text to display Player's Name")]
+        [SerializeField]
+        private Text playerNameText;
+
+        private PlayerManager target;
+
+        #endregion
+
+
+        #region MonoBehaviour Callbacks
+
+
+        #endregion
+
+
+        #region Public Methods
+
+        public void SetTarget(PlayerManager _target)
+        {
+            if(_target == null)
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> PlayMakerManager target for PlayerUI.SetTarget.", this);
+                return;
+            }
+
+            target = _target;
+            if (playerNameText != null)
+            {
+                playerNameText.text = target.photonView.Owner.NickName;
+            }
+        }
+
+        #endregion
+    }
+}
